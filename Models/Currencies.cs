7d1e6d75e879a84f65110ab43f0a5b1e3fb03cb4
Jonathan <jdazav.5@gmail.com>
2020-12-13 19:48:05 +1300

    Add project files.

---
+++ Models/Currencies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infraestructure.Api.ModelAnalizer;
+
+namespace Models
+{
+    [EntityInfo("adCurrency")]
+    public class Currencies
+    {
+        [EntityInfo("curId", false, true)]
+        public int Id { get; set; }
+
+        [EntityInfo("curCode", true, false)]
+        public string Code { get; set; }
+
+        [EntityInfo("curName")]
+        public string Name { get; set; }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infraestructure.Api.SPXDapperDataAccess;
+using Models;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SPXDPGenericRepository<Currencies> repo = new SPXDPGenericRepository<Currencies>("ArgosMain");
+
+            Currencies currency = new Currencies() {Code = "BIT", Id = 0, Name= "BITCOIN"};
+
+            var entidad = repo.GetAll();
+            
+            foreach (var registro in entidad)
+            {
+                Console.Write(registro.Name);
+                Console.Write(Environment.NewLine);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
